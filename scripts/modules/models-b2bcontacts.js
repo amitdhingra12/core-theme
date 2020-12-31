d94d145df0b5911d803ca0206a1f6f8b467e5649
Hrishikesh <hrishikesh>
2020-12-31 16:22:37 +0530

    COM-2600 Added Template for COM-2600
    COM-2600 Temporary Changes for Address modal popup
    
    COM-2600 Render Modal Popup
    
    COM-2600 Added working modal Popup
    
    COM-2600 Added Grid Selection Event mozugrid-view js
    
    COM-2600 Changes Grid Selection and Callback
    
    COM-2600 Fixed Quote.js
    
    COM-2600 Removed Account Name and Renamed files
    
    COM-2600 Added Button on seller side

---
+++ scripts/modules/models-b2bcontacts.js
@@ -0,0 +1,71 @@
+
+define(["modules/api", 'underscore', "modules/backbone-mozu", 'modules/models-address',  "hyprlive", "modules/models-product"], function (api, _, Backbone, AddressModels, Hypr, ProductModels, ReturnModels) {
+
+           
+    var QuoteItem = Backbone.MozuModel.extend({
+        relations: {
+            product: ProductModels.Product
+        },
+        helpers: ['uniqueProductCode'],
+        uniqueProductCode: function () {
+            //Takes into account product variation code
+            var self = this,
+                productCode = self.get('productCode');
+    
+            if (!productCode) {
+                productCode = (self.get('product').get('variationProductCode')) ? self.get('product').get('variationProductCode') : self.get('product').get('productCode');
+            }
+            return productCode;
+        },
+        toJSON: function () {
+            var j = Backbone.MozuModel.prototype.toJSON.apply(this, arguments);
+            if (j.parent) {
+                j.parent = j.parent.toJSON();
+            }
+            return j;
+        }
+    }),
+    
+        QuoteItemsList = Backbone.Collection.extend({
+            model: QuoteItem
+        }),
+    
+        B2bContact = Backbone.MozuModel.extend({
+            mozuType: 'quote',
+            idAttribute: 'id',
+            relations: {
+                items: QuoteItemsList
+            },
+            handlesMessages: true,
+            initialize: function () {
+                var self = this;
+            },
+            toJSON: function () {
+                var self = this,
+                    jsonItems = [];
+                this.get('items').each(function (item) {
+                    jsonItems.push(item.toJSON());
+                });
+                this.set('items', jsonItems);
+                var j = Backbone.MozuModel.prototype.toJSON.apply(this, arguments);
+                return j;
+            }
+        }),
+        B2bContactCollection = Backbone.MozuPagedCollection.extend({
+            mozuType: 'quotes',
+            defaults: {
+                pageSize: 5
+            },
+            relations: {
+                items: Backbone.Collection.extend({
+                    model: B2bContact
+                })
+            }
+        });
+    
+        return {
+                        B2bContactCollection: B2bContactCollection,
+                        B2bContact:B2bContact   
+                    };   
+
+});
