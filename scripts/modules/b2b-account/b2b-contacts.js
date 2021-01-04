d5821da09bf25fe98f9f0bc3cf358d40136a6c22
Hrishikesh <hrishikesh>
2021-01-04 17:40:26 +0530

    COM-2600 Render Modal Popup

--- scripts/modules/b2b-account/b2b-contacts.js
+++ scripts/modules/b2b-account/b2b-contacts.js
@@ -24,22 +24,30 @@ define([
 
     var B2bContactsView = Backbone.MozuView.extend({
         templateName: "modules/b2b-account/account-search/accounts-search",
-        initialize: function () {          
-            Backbone.MozuView.prototype.initialize.apply(this, arguments);
+        initialize: function () {       
+          Backbone.MozuView.prototype.initialize.apply(this, arguments);
         },
         render: function () {
             var self = this;
             Backbone.MozuView.prototype.render.apply(this, arguments);
             var collection = new B2bContactsGridCollectionModel({ autoload: true });          
-            this.initializeGrid(collection);
+            this.initializeGrid(collection);            
+            this.$modal = this.$('.modal');
         },      
         initializeGrid: function (collection) {
             var self = this;
             self._b2bContactGridView = new B2bContactsMozuGrid({
                 el: $('.mz-b2b-quotes-grid'),
                 model: collection
-            });
+            });           
         },
+        show: function() {
+            this.$el.modal('show');
+          },
+        renderView: function(template) {
+            this.$el.html(this.template);
+            this.$el.modal({show:true}); // dont show modal on instantiation
+          },
         registerRowActions: function () {
             var self = this;
             var rowActions = this.model.get('rowActions');
@@ -54,21 +62,105 @@ define([
        
     });  
 
-    
-    var B2bContactsGridCollectionModel = MozuGridCollection.extend({
-        mozuType: 'b2bcontacts',       
+    var B2bContactsGridCollectionModel= MozuGridCollection.extend({
+        mozuType: 'quotes',
+        defaultSort: 'submittedDate desc',
         columns: [
             {
-                index: 'accountName',
-                displayName: 'Account Name',
+                index: 'name',
+                displayName: 'Quote Name',
                 sortable: false
-            }           
-        ],relations: {
+            },
+            {
+                index: 'expirationDate',
+                displayName: 'Expiration Date',
+                sortable: true,
+                displayTemplate: function (value) {
+                    var date = "";
+                    if (value) {
+                        date = new Date(value).toLocaleString();
+                    }
+                    return date;
+                }
+            },
+            {
+                index: 'submittedDate',
+                displayName: 'Submitted Date',
+                sortable: true,
+                displayTemplate: function (value) {
+                    var date = "";
+                    if (value) {
+                        date = new Date(value).toLocaleString();
+                    }
+                    return date;
+                }
+            },
+            {
+                index: 'total',
+                displayName: 'Total',
+                sortable: false,
+                displayTemplate: function (amount) {
+                    return '$' + amount.toFixed(2);
+                }
+            },
+            {
+                index: 'status',
+                displayName: 'Status',
+                sortable: false
+            }
+        ],
+        rowActions: [
+            {
+                displayName: 'Edit Quote',
+                action: 'editQuote'
+            },
+            {
+                displayName: 'Delete Quote',
+                action: 'deleteQuote'
+            },
+            {
+                displayName: 'Copy Quote',
+                action: 'copyQuote'
+            },
+            {
+                displayName: 'Email Quote',
+                action: 'emailQuote'
+            }
+        ],
+        relations: {
             items: Backbone.Collection.extend({
-                model: B2bModels.B2bContact
+                model: QuoteModels.Quote
             })
-        }      
+        },
+        editQuote: function (e, row) {
+            var quoteId = row.get('id');
+            window.location = '/myaccount/quote/' + quoteId + '/edit';
+        },
+        deleteQuote: function () {
+            this.trigger('deleteQuoteView');
+        },
+        copyQuote: function () {
+            this.set('copyQuoteView');
+        },
+        emailQuote: function () {
+            this.set('emailQuoteView');
+        }
     });
+    
+    // var B2bContactsGridCollectionModel = MozuGridCollection.extend({
+    //     mozuType: 'b2bcontacts',       
+    //     columns: [
+    //         {
+    //             index: 'accountName',
+    //             displayName: 'Account Name',
+    //             sortable: false
+    //         }           
+    //     ],relations: {
+    //         items: Backbone.Collection.extend({
+    //             model: B2bModels.B2bContact
+    //         })
+    //     }      
+    // });
 
     return {
         'B2bContactsView': B2bContactsView     
--- scripts/modules/b2b-account/quotes.js
+++ scripts/modules/b2b-account/quotes.js
@@ -16,11 +16,12 @@ define([
     'modules/mozu-grid/mozugrid-pagedCollection',
     "modules/views-paging",
     'modules/editable-view',
-    "modules/models-quotes"], 
+    "modules/models-quotes",
+    "modules/b2b-account/b2b-contacts"], 
     function ($, api, _, Hypr, Backbone, HyprLiveContext,
     CustomerModels, CartModels, B2BAccountModels, ProductModalViews,
     ProductPicker, ProductModels, WishlistModels, MozuGrid, MozuGridCollection,
-    PagingViews, EditableView, QuoteModels) {
+    PagingViews, EditableView, QuoteModels, B2bContactpopup) {
         var nameFilter = "name cont ";
         var expirationDateFilter  = "expirationdate ge ";
         var timeComponent = "T00:00:00z";
@@ -61,7 +62,8 @@ define([
             return self.model;
         }
     });
-
+    
+    var modalView = new B2bContactpopup.B2bContactsView({model: CustomerModels.EditableCustomer.fromCurrent() });
     var QuotesView = Backbone.MozuView.extend({
         templateName: "modules/b2b-account/quotes/quotes",
         initialize: function () {
@@ -71,6 +73,7 @@ define([
             var self = this;
             Backbone.MozuView.prototype.render.apply(this, arguments);
             var collection = new QuotesGridCollectionModel({ autoload: true });
+            
             if (!self.model.get("b2bAccounts")) {
                 var b2bAccount = new B2BAccountModels.b2bAccounts({ pageSize: 200 });
                 b2bAccount.apiGet().then(function (accounts) {
