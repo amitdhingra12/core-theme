38670a9b924893d0632f356f540935b37f78a9fc
Hrishikesh <hrishikesh>
2021-01-22 17:01:23 +0530

    COM-2730 Added Popup for Child Account
    COM-2730 Added Api call create account and Binding to Account Dropdown
    
    COM-2730 Added * and Labels
    
    COM-2730 Added Validations

--- scripts/modules/b2b-account/child-account.js
+++ scripts/modules/b2b-account/child-account.js
@@ -21,7 +21,7 @@ define([
     CustomerModels, CartModels, B2BAccountModels, ProductModalViews,
     ProductPicker, ProductModels, WishlistModels, MozuGrid, MozuGridCollection,
     PagingViews, EditableView, QuoteModels) {
-        
+        var isValid =false;
     var childPopupView = Backbone.MozuView.extend({
         templateName: "modules/b2b-account/account-hierarchy/addchild-account-modal",
         initialize: function () {
@@ -37,7 +37,78 @@ define([
                 self.model.set("b2bAccounts", parentAccounts.attributes.accounts);
                     self.render();
             } 
+            $(document).ready(function() {
+                $(':input[type="submit"]').prop('disabled', true);
+            });
+            
+            $('[data-mz-action="validateIsRequired"]').on('focusout', function(e) {
+                 e.preventDefault();
+                 self.isValid =true;
+
+              if($("#firstName").val()==="")
+              {
+                self.isValid =false;
+                $('[data-mz-validationmessage-for="firstName"]').show().text(Hypr.getLabel('firstNameMissing'));
+              }
+              else
+              {
+                
+                $('[data-mz-validationmessage-for="firstName"]').show().text('');
+              }
+              if($("#lastName").val()==="")
+              {
+                self.isValid =false;
+                $('[data-mz-validationmessage-for="lastName"]').show().text(Hypr.getLabel('lastNameMissing'));
+              }
+              else
+              {
+                
+                $('[data-mz-validationmessage-for="lastName"]').show().text('');
+              }
+              if($("#companyName").val()==="")
+              {
+                self.isValid =false;
+                $('[data-mz-validationmessage-for="companyName"]').show().text(Hypr.getLabel('companyNameMissing'));
+              }
+              else
+              {
+               
+                $('[data-mz-validationmessage-for="companyName"]').show().text('');
+              }
+              if($("#email").val()==="" ||!self.validateEmail($("#email").val()))
+              {
+                self.isValid =false;
+                $('[data-mz-validationmessage-for="email"]').show().text(Hypr.getLabel('emailMissing'));
+              }
+              else
+              {
+                
+                $('[data-mz-validationmessage-for="email"]').show().text('');
+              }
+              if($(".mz-l-formfieldgroup-halfsize").val()==="")
+              {
+                self.isValid =false;
+                $('[data-mz-validationmessage-for="dropdown"]').show().text(Hypr.getLabel('accountMissing'));
+              }
+              else
+              {               
+                $('[data-mz-validationmessage-for="dropdown"]').show().text('');
+              }
+              if(self.isValid===true)
+              {
+                $(':input[type="submit"]').prop('disabled', false);
+              }
+              else
+              {
+                $(':input[type="submit"]').prop('disabled', true);
+              }
+            });
+           
         },
+         validateEmail:function($email) {
+            var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+            return emailReg.test( $email );
+          },
          renderView: function() {             
             var self = this;           
             this.$el.html(this.template);
@@ -49,8 +120,8 @@ define([
             {
                 "users":  [
                   {
-                      "emailAddress": $("#digital-fulfillment-email").val(),
-                      "userName": $("#companyName").val(),
+                      "emailAddress": $("#email").val(),
+                      "userName": $("#email").val(),
                       "firstName": $("#firstName").val(),
                       "lastName": $("#lastName").val(),
                       "localeCode": "en-US",
@@ -58,13 +129,16 @@ define([
                       "isActive": true
                   }
                 ],
+                "taxId":$( "#taxId" ).val(),
                 "parentAccountId": $( ".mz-l-formfieldgroup-halfsize" ).val(),
                 "companyOrOrganization":$( "#companyName" ).val(),
                 "accountType": "B2B"
             }));
             
              var apib2bAccount = new B2BAccountModels.b2bAccount(json);
-             apib2bAccount.apiModel.create();         
+             apib2bAccount.apiModel.create();  
+             window.location.reload();
+             $(':input[type="submit"]').prop('disabled', true);
           }       
     });
        return {
