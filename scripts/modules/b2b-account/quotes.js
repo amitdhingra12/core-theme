5ffd05f3d4bdabdb477648af9021322bc772f965
Hrishikesh <hrishikesh>
2021-02-02 19:27:02 +0530

    COM-3081 Added Quotes to the filter to Accomodate Space

--- scripts/modules/b2b-account/quotes.js
+++ scripts/modules/b2b-account/quotes.js
@@ -211,7 +211,7 @@ define([
         filterGrid: function (nameValue, dateValue, collection) {
             filterstring = "";
             if (nameValue !== "") {
-                nameValue = nameFilter + nameValue;
+                nameValue =  nameFilter+ '"'+ nameValue+'"';
                 filterstring = nameValue;
                 if (dateValue !== "") {
                     dateValue = expirationDateFilter + dateValue + timeComponent;
